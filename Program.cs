9207701c67da79d9957c0b37976570b83914e5ab
phoen <maryfredrick@gmail.com>
2025-04-10 02:31:37 +0100

    Trigger new  Build

--- Program.cs
+++ Program.cs
@@ -5,6 +5,6 @@ builder.WebHost.UseUrls("http://*:80");
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Jenkins pipline!");
+app.MapGet("/", () => "Hello new Jenkins pipline!");
 
 app.Run();
